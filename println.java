9c2872f7fff18495dae06f74af7a1cebb646caf8
Jonathan Sanchez <jcsanchez@72-166-205-248.resnet.adams.edu>
2022-02-04 18:20:29 -0700

    Print Java

---
+++ println.java
@@ -0,0 +1,11 @@
+public class println{
+    public static void main(String[] args) {
+        // \n takes the place of print'ln'
+        System.out.print("Look At Java Again\n");
+        //Print line = line break
+        System.out.println("Not to shabby");
+        // \t will tab ln
+        System.out.println("\t this is going well");
+
+    }
+}
